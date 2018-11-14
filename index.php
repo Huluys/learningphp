44bd4f0c57ee4881baee5633ac0c82839586128a
Aluno <aluno@edu.senac.br>
2018-11-14 21:43:53 -0200

    modificações

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1> 
+		<?php
+			$nome ="Neves";
+
+			echo $nome; // esse é um comentario
+			/*
+			esse comanterio é e várias linhas de texto e poder aumentar*/
+			#outro tipo de comentario
+?>
+</h1>
+</body>
+</html>
