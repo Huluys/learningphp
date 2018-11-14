3392d2c0a760d974526ff0fd76f52dbf2e27a5c8
Aluno <aluno@edu.senac.br>
2018-11-14 21:59:26 -0200

    modificações

--- index.php
+++ index.php
@@ -6,13 +6,22 @@
 <body>
 	<h1> 
 		<?php
-			$nome ="Neves";
+			$nome ="Matheus";
+			$sobre_nome ="Neves";
+			$nascimento ="05/06/1997";
+			
 
-			echo $nome; // esse é um comentario
+			/*echo $nome;
+			echo $sobre_nome;
+			echo $nascimento;*/
+			 // esse é um comentario
 			/*
 			esse comanterio é e várias linhas de texto e poder aumentar*/
 			#outro tipo de comentario
 ?>
+			<p><?php echo $nome; ?></p> 
+			<p><?php echo $sobre_nome; ?></p> 
+			<p><?php echo $nascimento; ?></p> 
 </h1>
 </body>
 </html>
